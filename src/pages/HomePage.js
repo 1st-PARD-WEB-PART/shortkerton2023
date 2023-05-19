f66736bd79127f6d492b7e28e6286b03b567f552
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-05-19 23:40:42 +0900

    seperate auth login from homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import NewQuestionForm from "../components/NewQuestionForm";
-import { GetCurrentUser, IsLogin } from "../services/AuthService";
+import { GetCurrentUser, GoogleLogin, IsLogin } from "../services/AuthService";
 
 const Button = styled.button`
 background : white;
 `;
-function HomePage() {
+const HomePage = async () => {
   return (
     <div>
-      <Button onClick={handleGoogleLogin}>로그인</Button>
+      <Button onClick={await GoogleLogin()}>로그인</Button>
       <div>
         {IsLogin()
           ? GetCurrentUser().displayName
