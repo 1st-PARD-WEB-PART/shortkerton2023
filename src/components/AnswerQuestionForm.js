6ef8c5461fcb77f8dc51dfb28d22cc27c1c01404
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-05-20 03:23:27 +0900

    answer question form

--- src/components/AnswerQuestionForm.js
+++ src/components/AnswerQuestionForm.js
@@ -3,31 +3,31 @@ import { TextField, Button, Alert } from '@mui/material';
 import { AddNewAnswer, AddNewQuestion } from '../services/DbService';
 
 
-const AnswerQuestionForm = ({user, questionId}) => {
-  const [answer, setAnswer] = useState('');
-  
+const AnswerQuestionForm = ({ user, questionData }) => {
+    const [answer, setAnswer] = useState('');
 
+    const handleClick = async () => {
+        const answerDocRef = await AddNewAnswer({ userId: user.uid, questionId: questionData.questionId, answer: answer });
+    };
 
-  const handleClick = async() => {
-    const answerDocRef = await AddNewAnswer({userId: user.uid, questionId: questionId, answer: answer});
-  };
-
-  const handleOnChange = (event) => {
-    const target = event.target;
-    console.log(target.id);
-    switch(target.id){
-      case "answer":
-        setAnswer(target.value);
-        break;
+    const handleOnChange = (event) => {
+        const target = event.target;
+        console.log(target.id);
+        switch (target.id) {
+            case "answer":
+                setAnswer(target.value);
+                break;
+        }
     }
-  }
 
-  return (
-    <>
-      <TextField id="answer" label={"answer"} variant="outlined" value={answer} onChange={(event) => handleOnChange(event)} />
-      <Button variant="contained" onClick={handleClick} disabled = {user == null}>제출하기</Button>
-    </>
-  );
+    return (
+        <>
+            {questionData.creatorId}의 질문
+            {questionData.question}
+            <TextField id="answer" label={"answer"} variant="outlined" value={answer} onChange={(event) => handleOnChange(event)} />
+            <Button variant="contained" onClick={handleClick} disabled={user == null}>제출하기</Button>
+        </>
+    );
 };
 
 export default AnswerQuestionForm;
