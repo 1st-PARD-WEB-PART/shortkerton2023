602c065ebec9f7e5653c31ea77ce6eb7663da0b1
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-05-19 23:39:03 +0900

    seperate auth login from homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,16 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useState } from "react";
-import { auth } from "../../src/firebase";
 import styled from 'styled-components';
 import NewQuestionForm from "../components/NewQuestionForm";
+import { IsLogin } from "../services/AuthService";
 
 const Button = styled.button`
 background : white;
 `;
 function HomePage() {
-  const [userData, setUserData] = useState(null);
-  function handleGoogleLogin() {
-    const provider = new GoogleAuthProvider(); // provider 구글 설정
-    signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
-      .then((data) => {
-        setUserData(data.user); // user data 설정
-        console.log(data); // console에 UserCredentialImpl 출력
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-
   return (
     <div>
       <Button onClick={handleGoogleLogin}>로그인</Button>
       <div>
-        {userData
+        {IsLogin()
           ? userData.displayName
           : ""}
 
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,16 +1,35 @@
-import { getAuth, signOut } from "firebase/auth"
+import { getAuth, signOut, signInWithPopup } from "firebase/auth"
 
 const user = getAuth();
 
+
 const GetCurrentUser = () => {
-    
     return user.currentUser;
 }
 
 const Logout = async () => {
-    if(GetCurrentUser() != null){
+    if (GetCurrentUser() != null) {
         await signOut();
     }
 }
 
-export {GetCurrentUser};
+const LoginWithProvider = async ({ provider }) => {
+    await signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
+        .then((data) => { // user data 설정
+            console.log(data); // console에 UserCredentialImpl 출력
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+} 
+
+const GoogleLogin = async () => {
+    const provider = new GoogleAuthProvider(); // provider 구글 설정
+    await LoginWithProvider({ provider });
+}
+
+const IsLogin = () => {
+    return GetCurrentUser() != null;
+}
+
+export { GetCurrentUser, Logout, GoogleLogin, IsLogin};
