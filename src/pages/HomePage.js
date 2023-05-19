9f6b1ca94c7a855255dbac51dd925d8e8bfe70b4
dbseend <shyoon840@gmail.com>
2023-05-19 21:33:17 +0900

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,14 @@
 import { PageContaienr } from "../styles/pageGeneral";
 import TextField from "../components/TextField";
-import styled from 'styled-components';
-
-const H1 = styled.div`
-    color : #FFFFFF;
-`
-
 
 function HomePage() {
 
     return (
-        <div>
-            <H1>질문을 입력하시오</H1>
-            <TextField />
-            <H1>답변을 입력하시오</H1>
-            <TextField />
+        <div>        
+            
         </div>
     );
+
 }
 
 export default HomePage;
