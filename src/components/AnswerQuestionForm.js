b71c2c0c1ba0753f9d854fa7d0d631a07901fec4
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-05-20 03:57:57 +0900

    answer question form edited for annoymous users

--- src/components/AnswerQuestionForm.js
+++ src/components/AnswerQuestionForm.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { TextField, Button, Alert } from '@mui/material';
-import { AddNewAnswer, AddNewQuestion } from '../services/DbService';
+import { AddNewAnswer, AddNewAnswerAsNotUser, AddNewQuestion } from '../services/DbService';
 
 
 
 const AnswerQuestionForm = ({ user, questionData }) => {
     const [answer, setAnswer] = useState('');
+    const [name, setName] = useState('');
 
-    const handleClick = async () => {
-        const answerDocRef = await AddNewAnswer({ userId: user.uid, questionId: questionData.questionId, answer: answer });
+    const handleClick = async ({userName}) => {
+        let answerDocRef;
+        if(user){
+            answerDocRef = await AddNewAnswer({ userId: user.uid, questionId: questionData.questionId, answer: answer });
+        } else{
+            answerDocRef = await AddNewAnswerAsNotUser({userName: userName ?? "anonymous", questionId: questionData.questionId, answer: answer});
+        }
     };
 
     const handleOnChange = (event) => {
@@ -18,16 +24,23 @@ const AnswerQuestionForm = ({ user, questionData }) => {
             case "answer":
                 setAnswer(target.value);
                 break;
+            case "name":
+                setName(target.value);
+                break;
         }
     }
-
-    
 
     return (
         <>
             {questionData.creatorId}의 질문
             {questionData.question}
             <TextField id="answer" label={"answer"} variant="outlined" value={answer} onChange={(event) => handleOnChange(event)} />
+            {
+                user == null
+                ?   <TextField id="name" label={"name"} variant="outlined" value={answer} onChange={(event) => handleOnChange(event)} />
+                : <></>
+            }
+
             <Button variant="contained" onClick={handleClick} disabled={user == null}>제출하기</Button>
         </>
     );
